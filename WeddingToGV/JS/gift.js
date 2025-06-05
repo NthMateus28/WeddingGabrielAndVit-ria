fd559d5c9b737e37bd00b2304a17edf37ddd0c4f
Mateus Loeser Reisdorfer <79878580+NthMateus28@users.noreply.github.com>
2025-06-05 13:12:37 -0300

    Update gift.js

--- WeddingToGV/JS/gift.js
+++ WeddingToGV/JS/gift.js
@@ -1,12 +1,13 @@
 // Configuração do Firebase (usando compat)
 const firebaseConfig = {
-    apiKey: "AIzaSyAFDeiAptyKrAnMLrq93GAE1ayzXz3wDj4",
-    authDomain: "lista-presentes-casament-d2207.firebaseapp.com",
-    databaseURL: "https://lista-presentes-casament-d2207-default-rtdb.firebaseio.com",
-    projectId: "lista-presentes-casament-d2207",
-    storageBucket: "lista-presentes-casament-d2207.appspot.com",
-    messagingSenderId: "942457963760",
-    appId: "1:942457963760:web:1c41d0e5f52c37b6e82e35"
+  apiKey: "AIzaSyBaMj4lQzmevuWwqio-udKGLtMT31hW5Bw",
+  authDomain: "lista-presentes-casament-d2207.firebaseapp.com",
+  databaseURL: "https://lista-presentes-casament-d2207-default-rtdb.firebaseio.com",
+  projectId: "lista-presentes-casament-d2207",
+  storageBucket: "lista-presentes-casament-d2207.firebasestorage.app",
+  messagingSenderId: "942457963760",
+  appId: "1:942457963760:web:1c41d0e5f52c37b6e82e35",
+  measurementId: "G-GGPR8564P5"
   };
   
   firebase.initializeApp(firebaseConfig);
